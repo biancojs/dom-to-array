0ea2f08c54fb69df606e05e0f600f91e83ea132b
Gianluca Guarini <gianluca.guarini@gmail.com>
2016-09-18 01:40:33 +0200

    updated: avoid to bundle the package dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,5 @@
-import resolve from 'rollup-plugin-node-resolve'
-
 export default {
   entry: 'index.next.js',
-  plugins: [
-    resolve({
-      jsnext: true
-    })
-  ],
   targets: [
     {
       dest: 'index.js',
