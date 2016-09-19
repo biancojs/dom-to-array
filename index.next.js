01985cce4948beacacf0dfc71d39337e9c860aeb
Nils Solanki <nils.solanki@dreipol.ch>
2016-09-19 14:19:35 +0200

    [simplify-array] Remove trailing spaces

--- index.next.js
+++ index.next.js
@@ -13,7 +13,7 @@ export default function domToArray(els) {
       // if it's a single node
       // it will be returned as "array" with one single entry
       return [els]
-  }  
+  }
   // this object could be looped out of the box
   return els
 }
